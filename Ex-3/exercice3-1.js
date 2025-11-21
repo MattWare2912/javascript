4a6e403fa9d7a9b38f5afd8ed1c044ac77a93810
MattWare <schlitmatthias@gmail.com>
2025-11-21 10:50:39 +0100

    js

---
+++ Ex-3/exercice3-1.js
@@ -0,0 +1,23 @@
+let arr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let number of arr) {
+    sum += number;
+
+}
+
+console.log("la somme est " + sum);
+
+let arr2 = [100, 101, 102];
+let sum2 = 0;
+
+for (let number of arr2) {
+sum2 += number;
+}
+console.log("La somme est " + sum2);
+
+/* for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+}
+
+console.log("somme = " + sum); */
